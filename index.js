c83fd002da637fd7f6bd96491c9cd2ad8fd79a79
guanyu <zhuliangcan@foresee.com.cn>
2018-08-28 14:20:20 +0800

    publish 2.0.0

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ export default {
 	config: config,
 	load: (cb) => {
 		require.ensure([], require => {
-			cb(require('./component'), require('./action'), require('./reducer'))
+			cb(require('./component'), require('./action'), require('./reducer'), require('./data'), require('./config'))
 		}, "app-test")
 	}
 }
